88316f8d467afed8e203a26bd543bc7b4ea13871
테스트 <20191476@office.dongseo.ac.kr>
2023-12-08 03:08:19 +0900

    수정완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from "./components/Header";
 import './App.css';
 
-function App() {
-  return (
-    <div>App
+const App = () => {
+  return <div className="container">
+    <div className="app-wrapper">
+      <div>
+        <Header />
+      </div>
     </div>
-  );
-}
+  </div>
+  
+};
 
 export default App;
